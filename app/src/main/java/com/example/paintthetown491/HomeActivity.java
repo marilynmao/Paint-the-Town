ee6e3296b811def162a533e568167df0b9f68cb9
jcampos13 <39930978+jcampos13@users.noreply.github.com>
2020-09-09 22:26:55 -0700

    navigation bar
    pt1

--- app/src/main/java/com/example/paintthetown491/HomeActivity.java
+++ app/src/main/java/com/example/paintthetown491/HomeActivity.java
@@ -1,32 +1,49 @@
 package com.example.paintthetown491;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity
 {
+    DrawerLayout dLayout;
+    ActionBarDrawerToggle actionToggle;
+    Toolbar toolBar;
+    NavigationView navView;
     private Button signoutButton;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        
-        signoutButton=findViewById(R.id.logoutButton);
-        signoutButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                logOut(view);
-            }
-        });
+        dLayout=findViewById(R.id.drawer);
+        toolBar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolBar);
+
+        navView=findViewById(R.id.navView);
+        actionToggle=new ActionBarDrawerToggle(this,dLayout,toolBar,R.string.open,R.string.close);
+        dLayout.addDrawerListener(actionToggle);
+        actionToggle.setDrawerIndicatorEnabled(true);
+        actionToggle.syncState();
+
+        //signoutButton=findViewById(R.id.logoutButton);
+        //signoutButton.setOnClickListener(new View.OnClickListener()
+        //{
+        //    @Override
+        //    public void onClick(View view)
+        //    {
+        //        logOut(view);
+        //    }
+        //});
     }
 
     public void logOut(View view)
