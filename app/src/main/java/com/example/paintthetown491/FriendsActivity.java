6f508a216574a9e075ec6a12594df6b9f9f119a8
Collin Doss <46690794+YungMayne@users.noreply.github.com>
2020-10-12 15:04:04 -0700

    Merge branch 'master' of C:\Users\colli\Documents\GitHub\Paint-the-Town with conflicts.

--- app/src/main/java/com/example/paintthetown491/FriendsActivity.java
+++ app/src/main/java/com/example/paintthetown491/FriendsActivity.java
@@ -14,7 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 public class FriendsActivity extends Fragment
 {
     private RecyclerView FriendsRecycler;
-    private FriendsListAdapter FlAdapter;
+    private ProfilesAdapter ProfAdapter;
     private RecyclerView.LayoutManager layoutManager;
 
     @Nullable
@@ -25,27 +25,28 @@ public class FriendsActivity extends Fragment
         View view=inflater.inflate(R.layout.frag_events, container, false);
 
         //dummy data to test the recyclerview
-        //ArrayList<FriendsListItem> sampleData=new ArrayList<>();
-        //sampleData.add(new FriendsListItem(R.drawable.ic_baseline_event_24,"Night out with the BOYZZZ","starting on: "+"12/12/2020", "created by: "+"Julian Campos"));
-        //sampleData.add(new FriendsListItem(R.drawable.ic_baseline_event_24,"Night out with the GIRLZZZ","starting on: "+"12/22/2020","created by:"+"Julian Campos"));
+        ArrayList<FriendsListItem> sampleData=new ArrayList<>();
+        sampleData.add(new FriendsListItem(R.drawable.ic_baseline_event_24,"Night out with the BOYZZZ","starting on: "+"12/12/2020", "created by: "+"Julian Campos"));
+        sampleData.add(new FriendsListItem(R.drawable.ic_baseline_event_24,"Night out with the GIRLZZZ","starting on: "+"12/22/2020","created by:"+"Julian Campos"));
 
-        //FriendsRecycler=view.findViewById(R.id.events);
-        //FriendsRecycler.setHasFixedSize(true);
-        //layoutManager=new LinearLayoutManager(getContext());
-        //FlAdapter =new FriendsListAdapter(sampleData);
-        //FriendsRecycler.setLayoutManager(layoutManager);
-        //FriendsRecycler.setAdapter(FlAdapter);
-        //FlAdapter.setOnItemClickListener(new EventAdapter.OnItemClickListener()
-        //{
+        //havent finished yet because we havent decided how to manage the profiles for friends list
+        FriendsRecycler=view.findViewById(R.id.events);
+        FriendsRecycler.setHasFixedSize(true);
+        layoutManager=new LinearLayoutManager(getContext());
+        ProfAdapter =new ProfilesAdapter(sampleData);
+        FriendsRecycler.setLayoutManager(layoutManager);
+        FriendsRecycler.setAdapter(ProfAdapter);
+        ProfAdapter.setOnItemClickListener(new EventAdapter.OnItemClickListener()
+        {
             //handles what happens when an item from the recyclerview is clicked
-        //    @Override
-        //    public void onItemClick(int position)
-        //    {
-        //        //TODO:open up the event page
-        //        startActivity(new Intent(getContext(),EventPopUpActivity.class));
-        //        System.out.println("CLICKED!");
-        //    }
-        //});
+            @Override
+            public void onItemClick(int position)
+            {
+                //TODO:open up the event page
+               startActivity(new Inten t(getContext(),EventPopUpActivity.class));
+                System.out.println("CLICKED!");
+            }
+        });
 
         return view;
     }
