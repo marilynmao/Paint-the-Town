3e0d492056b833f892d4c59f28371c6fc588ee3e
Collin Doss <46690794+YungMayne@users.noreply.github.com>
2020-10-18 20:44:52 -0700

    Made FriendsActivity consistent with EventActivity class. friendsRecycler still needs proper argument, and database needs to be connected.

--- app/src/main/java/com/example/paintthetown491/FriendsActivity.java
+++ app/src/main/java/com/example/paintthetown491/FriendsActivity.java
@@ -4,51 +4,144 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 //class that holds elements needed for our events fragment
 public class FriendsActivity extends Fragment
 {
-    private RecyclerView FriendsRecycler;
-    private ProfilesAdapter ProfAdapter;
+    private RecyclerView friendsRecycler;
+    private ProfilesAdapter profAdapter;
     private RecyclerView.LayoutManager layoutManager;
+    private DatabaseReference eventRef;
+    private ArrayList<String> userIDs;
+    private ArrayList<User> users;
+
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
-    {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
         //the inflate() method takes the layout you wanna show as the first parameter
-        View view=inflater.inflate(R.layout.frag_events, container, false);
+        final View view = inflater.inflate(R.layout.frag_events, container, false);
 
-        //dummy data to test the recyclerview
-        ArrayList<FriendsListItem> sampleData=new ArrayList<>();
-        sampleData.add(new FriendsListItem(R.drawable.ic_baseline_event_24,"Night out with the BOYZZZ","starting on: "+"12/12/2020", "created by: "+"Julian Campos"));
-        sampleData.add(new FriendsListItem(R.drawable.ic_baseline_event_24,"Night out with the GIRLZZZ","starting on: "+"12/22/2020","created by:"+"Julian Campos"));
+        //holds the event IDs for each user
+        userIDs = new ArrayList<String>();
+        //holds the events loaded from firebase
+        users = new ArrayList<User>();
 
-        //havent finished yet because we havent decided how to manage the profiles for friends list
-        FriendsRecycler=view.findViewById(R.id.events);
-        FriendsRecycler.setHasFixedSize(true);
-        layoutManager=new LinearLayoutManager(getContext());
-        ProfAdapter =new ProfilesAdapter(sampleData);
-        FriendsRecycler.setLayoutManager(layoutManager);
-        FriendsRecycler.setAdapter(ProfAdapter);
-        ProfAdapter.setOnItemClickListener(new EventAdapter.OnItemClickListener()
+        //listener for the event IDs in firebase
+        ValueEventListener eventIdValListener = new ValueEventListener()
         {
-            //handles what happens when an item from the recyclerview is clicked
             @Override
-            public void onItemClick(int position)
+            public void onDataChange(@NonNull DataSnapshot snapshot)
             {
-                //TODO:open up the event page
-               startActivity(new Inten t(getContext(),EventPopUpActivity.class));
-                System.out.println("CLICKED!");
+                userIDs.clear();
+                //check to see if firebase returned anything
+                if (snapshot.exists())
+                {
+                    //iterate through each child returned
+                    for (DataSnapshot e : snapshot.getChildren())
+                    {
+                        //holds the event ID as a string
+                        String event = e.getValue(String.class);
+                        //adds it to the list
+                        userIDs.add(event);
+                    }
+                    //notifies the adapter of any changes
+                    profAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error)
+            {
+            }
+        };
+
+        //querying the specific user's list of friends
+        Query query = FirebaseDbSingleton.getInstance().dbRef.child("User").child(FirebaseDbSingleton.getInstance().user.getUid()).child("friends");
+        //attaching the value listener
+        query.addValueEventListener(eventIdValListener);
+
+        //now that we have the event IDs saved, we need to look at the Event table for each of them
+        eventRef = FirebaseDbSingleton.getInstance().dbRef.child("User");
+        //listener for the values we want
+        eventRef.addValueEventListener(new ValueEventListener()
+        {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot)
+            {
+                //check that firebase returned something
+                if (snapshot.exists())
+                {
+                    //iterate through each child returned
+                    for (DataSnapshot ds : snapshot.getChildren())
+                    {
+                        String s=ds.getKey();
+
+                        if(ds.getKey().charAt(0)=='-')
+                        {
+                            //removes a "-" character appended to the beginning of each key
+                            s = ds.getKey().substring(1);
+                        }
+
+                        //sees if the arraylist contains this child
+                        if (userIDs.contains(s))
+                        {
+                            //create the friends object with the properties returned from firebase
+                            User f = new User();
+                            //add it to the arraylist that goes into the adapter
+                            users.add(f);
+                        }
+                    }
+                }
+                //notifies the adapter of any changes
+                profAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error)
+            {
+            }
+
+            //used to convert a Iterable (type returned from firebase) to an arraylist
+            public ArrayList<String> getCollectionFromIterable(Iterable<DataSnapshot> itr)
+            {
+                ArrayList<String> participants = new ArrayList<String>();
+                for (DataSnapshot id : itr)
+                {
+                    participants.add(id.toString());
+                }
+                return participants;
             }
         });
 
+
+    //events needs to be replaced by Users or userID
+        friendsRecycler = view.findViewById(R.id.events);
+        friendsRecycler.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(getContext());
+        profAdapter = new ProfilesAdapter(users);
+        friendsRecycler.setLayoutManager(layoutManager);
+        friendsRecycler.setAdapter(profAdapter);
+
+        // set on click listener for create event button to up create event page
+
+
+
         return view;
     }
-
 }
